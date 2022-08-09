273fa009528141dc69901ce88b4a866321a904eb
tallesChristian <72113346+tallesChristian@users.noreply.github.com>
2022-08-09 17:32:52 -0300

    Adicionar arquivos de projeto.

---
+++ Financa/Models/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace Financa.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public string Tel { get; set; }
+        public string Email { get; set; }
+    }
+}
