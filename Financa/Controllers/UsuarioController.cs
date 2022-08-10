d1988c1527bb95444d31b5216f648e714aeb4767
tallesChristian <72113346+tallesChristian@users.noreply.github.com>
2022-08-09 21:24:55 -0300

    Adicionando Controller

---
+++ Financa/Controllers/UsuarioController.cs
@@ -0,0 +1,27 @@
+﻿using Financa.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Financa.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private static List<Usuario> usuarios = new List<Usuario>();
+
+        [HttpPost]
+        public void AdicionaUsuario([FromBody]Usuario usuario)
+        {
+            usuarios.Add(usuario);
+            Console.WriteLine(usuario.Name);
+        }
+
+        [HttpGet]
+        public IEnumerable<Usuario> RecuperaUsuario()
+        {
+            return usuarios;
+        }
+    }
+}
